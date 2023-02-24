e0dc717caa993a5a46fcf02a6964b1058d40f871
Lucas Aquino <lucasaquino350@gmail.com>
2023-02-24 17:04:25 -0300

    inicia projeto

---
+++ src/main/java/br/com/lashDesign/clienteprocedimento/ClienteProcedimentoApplication.java
@@ -0,0 +1,13 @@
+package br.com.lashDesign.clienteprocedimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClienteProcedimentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteProcedimentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lashDesign/clienteprocedimento/ClienteProcedimentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lashDesign.clienteprocedimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteProcedimentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
