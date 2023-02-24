9b7f552566e969c315ce5507f7f4d3ccc0d441c7
Lucas Aquino <lucasaquino350@gmail.com>
2023-02-24 17:32:30 -0300

    implementa api de teste

--- src/main/java/br/com/lashDesign/clienteprocedimento/ClienteProcedimentoApplication.java
+++ src/main/java/br/com/lashDesign/clienteprocedimento/ClienteProcedimentoApplication.java
@@ -2,12 +2,21 @@ package br.com.lashDesign.clienteprocedimento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ClienteProcedimentoApplication {
 
+	@GetMapping
+	public String getHomeTeste() {
+		return "API - LASH DESIGN";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ClienteProcedimentoApplication.class, args);
 	}
-
 }
